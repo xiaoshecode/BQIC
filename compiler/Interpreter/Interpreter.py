d849c433b154fcfec1e3e59599cc6731e72489e9
xiaoshe <shejp20@gmail.com>
2025-02-19 16:23:58 +0800

    initialize the sequence

---
+++ compiler/Interpreter/Interpreter.py
@@ -0,0 +1,19 @@
+from DDS_Seq import DDS, Seq
+from DDS_config import *
+
+
+# first, initialize the sequence
+DDS.VERBOSE = True
+# print(DDS.All)
+# print(len(DDS.List))
+# detection
+seq = Seq().S7(4).S13(4)
+# print(seq)
+rd_seq = seq.compile()
+print(seq.compile())
+# print(seq.compile(4))
+print("序列准备好了")
+
+
+
+
